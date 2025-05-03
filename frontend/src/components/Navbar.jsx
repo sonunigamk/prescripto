a4a80cee310791f6e5cb8c5226719cfbffd964a3
Sonu Kumar <sonunigam0382002@gmail.com>
2025-05-03 12:54:03 +0530

    frontend part completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400' >
             <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
-        {/* <div className="log flex gap-1 px-1 justify-center items-center">
+            {/* <div className="log flex gap-1 px-1 justify-center items-center">
         <FaLaptopMedical  className=' text-blue-600 text-4xl rounded-2xl'/>
         <h1 className='font-bold text-3xl text-blue-950 '>Prescripto</h1>
         </div> */}
-            
+
             <ul className='hidden md:flex items-start gap-5 font-medium'>
                 <NavLink to='/'>
                     <li className='py-1'>Home</li>
@@ -45,20 +45,37 @@ const Navbar = () => {
                             <img className='w-2.5' src={assets.dropdown_icon} alt="" />
                             <div className=' absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                                    <p onClick={()=>navigate('my-profile')}
-                                    className='hover:text-black cursor-pointer'>My Profile</p>
-                                    <p 
-                                    onClick={()=>navigate('my-appointments')}
-                                    className='hover:text-black cursor-pointer'>My Appointments</p>
-                                    <p  onClick={()=>setToken(false)}
-                                    
-                                className='hover:text-black cursor-pointer'>Logout</p>
+                                    <p onClick={() => navigate('my-profile')}
+                                        className='hover:text-black cursor-pointer'>My Profile</p>
+                                    <p
+                                        onClick={() => navigate('my-appointments')}
+                                        className='hover:text-black cursor-pointer'>My Appointments</p>
+                                    <p onClick={() => setToken(false)}
+
+                                        className='hover:text-black cursor-pointer'>Logout</p>
                                 </div>
                             </div>
                         </div>
                         : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light md:block'>Create Account</button>
 
                 }
+                <img onClick={() => setShowMenu(true)}
+                    className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+
+                {/* mobile  menu */}
+
+                <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                    <div className='flex items-center justify-between px-5 py-6'>
+                        <img className='w-36' src={assets.logo} alt="" />
+                        <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+                    </div>
+                    <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                        <NavLink onClick={()=>setShowMenu(false)} to="/"  ><p className="px-4 py-2 rounded inline-block">Home</p></NavLink>
+                        <NavLink onClick={()=>setShowMenu(false)} to="/doctors"><p className="px-4 py-2 rounded inline-block">All Doctors</p></NavLink>
+                        <NavLink onClick={()=>setShowMenu(false)} to="/about"><p className="px-4 py-2 rounded inline-block">About</p></NavLink>
+                        <NavLink onClick={()=>setShowMenu(false)} to="/contact"><p className="px-4 py-2 rounded inline-block">Contact</p></NavLink>
+                    </ul>
+                </div>
 
             </div>
         </div>
