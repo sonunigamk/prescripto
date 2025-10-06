b4f6a05a05376c732729525c84b76a46edb44fab
Sonu Kumar <sonunigam0382002@gmail.com>
2025-10-06 18:16:00 +0530

    date formated

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,17 +9,15 @@ const MyAppointments = () => {
 
   const [appointments, setAppointment] = useState([]);
 
-  const months = [
-    "",
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-  ];
-
   const navigate = useNavigate()
+
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split("_");
     return `${dateArray[0]} ${months[Number(dateArray[1])]} ${dateArray[2]}`;
   };
+
 
   const getUserAppointments = async () => {
     try {
