7b368fc7ca3257edd55c934864ee72928ab8ed11
Sonu Kumar <sonunigam0382002@gmail.com>
2025-11-19 10:58:46 +0530

    url added

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,40 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 /////------app config-------/////
 
-const app = express()
-const port = process.env.PORT || 4000 
+const app = express();
+const PORT = process.env.PORT || 4000;
 connectDB();
-connectCloudinary()
-
+connectCloudinary();
 
 /// ------middlewares-------///
-app.use(express.json())
-app.use(cors({
+app.use(express.json());
+app.use(
+  cors({
     origin: [
-        'http://localhost:5173', 
-        'http://localhost:5174'  
+      "https://appointment-booker-app.vercel.app/",
+      "https://prescripto-admin-beta-drab.vercel.app/",
     ],
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
-
 
 ///-------api endpoint------///
 
-app.use('/api/admin',adminRouter)
-app.use('/api/doctor',doctorRouter)
-app.use('/api/user',userRouter)
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Api working great");
+});
 
-app.get('/',(req,res)=>{
-    res.send('Api working great')
-})
-
-
-
-app.listen(port,()=> console.log("server is running on port no:",port))
+app.listen(PORT, () => console.log("server is running on port no:", PORT));
